b56dea608bf742667b3b5a44125e08a98a103728
MagaFreelancer <ban0le4@gmail.com>
2026-02-22 06:02:33 +0300

    chore: Добавил eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+
+
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
+  {
+    rules: {
+      "no-console": "warn", //чтобы не забывать удалять console.log
+      "eqeqeq": "warn", //чтобы не забывать использовать строгое сравнение
+      "curly": "warn", //чтобы не забывать использовать фигурные скобки в блоках if, else, for, while и do-while
+      "no-else-return": "warn", //чтобы не забывать удалять else после return
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
 
-export default eslintConfig;
+])
+
+export default eslintConfig
